d0f2eaa152ebc048e8d80b8190fd4bb2b615d488
Bryce Lively <bryce.lively@gmail.com>
2015-11-20 07:08:42 -0500

    rake doc:app and removed minimized bootstrap

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( css/bootstrap.min.css )
-Rails.application.config.assets.precompile += %w( jquery.imagemapster.js )
-Rails.application.config.assets.precompile += %w( js/bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( js/bootstrap.js )
